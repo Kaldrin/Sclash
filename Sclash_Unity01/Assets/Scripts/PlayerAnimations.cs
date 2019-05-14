270dfbd2e36388868dd1b3d22fe2f599cd7352e1
Ruyuik <callotv@hotmail.fr>
2019-05-14 17:24:29 +0200

    [REWORK] Enemy facing

--- Sclash_Unity01/Assets/Scripts/PlayerAnimations.cs
+++ Sclash_Unity01/Assets/Scripts/PlayerAnimations.cs
@@ -15,12 +15,6 @@ public class PlayerAnimations : MonoBehaviour
     [SerializeField]
     float speedForWalking = 0.5f;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
     // Update is called once per frame
     void Update()
     {
