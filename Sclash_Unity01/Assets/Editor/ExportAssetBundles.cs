b544f18e27c89538e5db8e61cc6adeb0bd52e1a2
Ruyuik <callotv@hotmail.fr>
2021-03-23 15:50:25 +0100

    Asset bundle test

---
+++ Sclash_Unity01/Assets/Editor/ExportAssetBundles.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class ExportAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundle")]
+    static void ExportRessource()
+    {
+        string folderName = "AssetBundles";
+        string filePath = Path.Combine(Application.streamingAssetsPath, folderName);
+
+        BuildPipeline.BuildAssetBundles(filePath, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+
+        AssetDatabase.Refresh();
+    }
+}
