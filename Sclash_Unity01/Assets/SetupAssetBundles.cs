187271fe448632cbdb1545168709edef32c5d919
Ruyuik <callotv@hotmail.fr>
2021-03-25 00:39:11 +0100

    Asset bundle part 3

---
+++ Sclash_Unity01/Assets/SetupAssetBundles.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetupAssetBundles : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject loader = new GameObject();
+        DontDestroyOnLoad(loader);
+        loader.AddComponent<LoadAssetBundles>();
+    }
+}
